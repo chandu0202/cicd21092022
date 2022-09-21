949507b718e49ee8beccee0442a9c328e995fc09
chandu0202 <113936980+chandu0202@users.noreply.github.com>
2022-09-21 09:40:11 +0530

    Add files via upload

---
+++ chandu/chandu/src/main/java/com/chandu/chandu/ChanduApplication.java
@@ -0,0 +1,13 @@
+package com.chandu.chandu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanduApplication.class, args);
+	}
+
+}
---
+++ chandu/chandu/src/test/java/com/chandu/chandu/ChanduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.chandu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
